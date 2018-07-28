27c451f8953157da88f18644efb61b1c81f2ccd7
Kyle Gruber <kylegruber@utexas.edu>
2018-07-28 09:26:14 -0400

    graph and table pulls from api

--- web/index.js
+++
@@ -1,9 +0,0 @@
-console.log("test");
-$(document).ready(function() {
-    console.log("test");
-    $.ajax({
-        url: "http://localhost:8081/"
-    }).then(function(data) {
-        console.log(JSON.stringify(data));
-    });
-});
