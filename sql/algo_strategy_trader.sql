c9ca2a048b601cc04bc80c3ecc3204626a86a453
pigottcd <pigottcd@gmail.com>
2018-07-30 20:37:48 +0000

    sql fix pt 2

--- sql/algo_strategy_trader.sql
+++ sql/algo_strategy_trader.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `algo_strategy_trader` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `algo_strategy_trader`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: algo_strategy_trader
@@ -106,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-30 20:34:33
+-- Dump completed on 2018-07-30 20:37:41
