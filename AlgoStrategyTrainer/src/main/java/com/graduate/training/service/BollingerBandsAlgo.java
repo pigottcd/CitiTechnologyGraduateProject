e7802bc1a473fcb568804861da56a8a64ffc5f7f
pigottcd <pigottcd@gmail.com>
2018-08-05 19:44:06 +0000

    initial strat copied from moving averages

--- AlgoStrategyTrainer/src/main/java/com/graduate/training/service/BollingerBandsAlgo.java
+++ AlgoStrategyTrainer/src/main/java/com/graduate/training/service/BollingerBandsAlgo.java
@@ -3,6 +3,9 @@ package com.graduate.training.service;
 import com.graduate.training.entities.Order;
 import com.graduate.training.entities.Strategy;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class BollingerBandsAlgo extends StrategyAlgo {
 
     private int windowLength;
@@ -21,6 +24,45 @@ public class BollingerBandsAlgo extends StrategyAlgo {
     }
 
     public Order runStrategy(PriceFeedService priceFeed){
+        Order newOrder = null;
+        String ticker = getTicker();
+
+        List<Double> currentRange = priceFeed.getPriceRange(ticker, shortPeriod);
+        if (currentRange.size() < longPeriod) {
+            System.out.println("warming up, currentLongRangeSize(): " + currentLongRange.size());
+            return newOrder;
+        }
+
+        //get Averages
+        double currentShortAverage = getAverage(currentShortRange);
+        double currentLongAverage = getAverage(currentLongRange);
+        System.out.println("shortAverage: " + currentShortAverage + ", longAverage: " + currentLongAverage);
+
+        double price = priceFeed.getCurrentPrice(ticker);
+        if ((shortAverage > longAverage)&&(currentShortAverage < currentLongAverage)){
+            if (pastBuy == null) {
+                pastBuy = false;
+            } else if (!pastBuy) {
+                return newOrder;
+            }
+            pastBuy = false;
+            newOrder = new Order(false, price,getStrategy().getQuantity(), ticker, LocalDateTime.now(), getId());
+            System.out.println("New Sell Order:" + newOrder.toString());
+        }
+        else if ((longAverage > shortAverage)&&(currentLongAverage < currentShortAverage)) {
+            if (pastBuy == null) {
+                pastBuy = true;
+            } else if (pastBuy) {
+                return newOrder;
+            }
+            pastBuy = true;
+            newOrder = new Order(true, price, getStrategy().getQuantity(), ticker, LocalDateTime.now(), getId());
+            System.out.println("New Buy Order:" + newOrder.toString());
+        }
+
+        shortAverage = currentShortAverage;
+        longAverage = currentLongAverage;
+        return newOrder;
        return null;
     }
 
